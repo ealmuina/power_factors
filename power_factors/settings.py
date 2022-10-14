3a9a44f2f7152456ae97d901f1ca5ac4939f98de
Eddy Almuiña <almuina.e@gmail.com>
2022-10-14 21:53:27 +0200

    Cleanup

--- power_factors/settings.py
+++ power_factors/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+em*pp^0v7nybibvu3sp)z^u2kbrf$ru3hb8u3ieqr(87e1a1s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
